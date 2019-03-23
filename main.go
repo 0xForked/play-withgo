9ded7c653da51f91b3e2b958ee1371b8804816bd
A. A. Sumitro <aasumitro@gmail.com>
2019-03-24 01:11:59 +0800

    update main.go

--- main.go
+++ main.go
@@ -2,31 +2,8 @@ package main
 
 import (
 	"fmt"
-	"net/http"
-
-	"github.com/gorilla/mux"
 )
 
-func home(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to aasumtro</h1>")
-}
-
-func blog(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	fmt.Fprint(w, "<h1>Welcome to my blog</h1>")
-}
-
-func notFound(w http.ResponseWriter, r *http.Request) {
-	w.Header().Set("Content-Type", "text/html")
-	w.WriteHeader(http.StatusNotFound)
-	fmt.Fprint(w, "<h1>404</h1>")
-}
-
 func main() {
-	route := mux.NewRouter()
-	route.NotFoundHandler = http.HandlerFunc(notFound)
-	route.HandleFunc("/", home)
-	route.HandleFunc("/blog", blog)
-	http.ListenAndServe(":8080", route)
+	fmt.Println("Hello, World!")
 }
