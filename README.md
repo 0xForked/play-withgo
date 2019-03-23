045ac21efdd113257225a025fa135b658784ee66
aasumitro <aasumitro@gmail.com>
2019-03-23 21:38:33 +0800

    update

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # Learn Web Development with Go Lang!
 
+- using gorilla mux
--- main.go
+++ main.go
@@ -1,7 +1,32 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
+
+func home(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Welcome to aasumtro</h1>")
+}
+
+func blog(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	fmt.Fprint(w, "<h1>Welcome to my blog</h1>")
+}
+
+func notFound(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/html")
+	w.WriteHeader(http.StatusNotFound)
+	fmt.Fprint(w, "<h1>404</h1>")
+}
 
 func main() {
-	fmt.Println("Hello World")
+	route := mux.NewRouter()
+	route.NotFoundHandler = http.HandlerFunc(notFound)
+	route.HandleFunc("/", home)
+	route.HandleFunc("/blog", blog)
+	http.ListenAndServe(":8080", route)
 }
